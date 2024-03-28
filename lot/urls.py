3eb2e89c13623c693f15c64e760c51d43f0936ba
BriCusack <130425167+BriCusack@users.noreply.github.com>
2024-03-28 13:55:58 +1100

    Update urls.py
    pattern to path

--- lot/urls.py
+++ lot/urls.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from django.urls import include, re_path
+from django.urls import path, re_path
 
 from . import views
 
-urlpatterns = patterns("",
+urlpatterns = path("",
     re_path(r"^login/(?P<uuid>[\da-f]{8}-([\da-f]{4}-){3}[\da-f]{12})/$", views.LOTLogin.as_view(), name="login"),
 )
