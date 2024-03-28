2b6cf0d4caad867b6bbaefea20cec78c4a17e4d1
BriCusack <130425167+BriCusack@users.noreply.github.com>
2024-03-28 14:00:34 +1100

    Update urls.py
    login path

--- lot/urls.py
+++ lot/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, re_path
 
 from . import views
 
-urlpatterns = path("",
-    re_path(r"^login/(?P<uuid>[\da-f]{8}-([\da-f]{4}-){3}[\da-f]{12})/$", views.LOTLogin.as_view(), name="login"),
-)
+urlpatterns = [
+    path("login/<uuid:uuid>/", views.LOTLogin.as_view(), name="login"),
+]
+
